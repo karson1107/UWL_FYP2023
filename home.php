b78ba65f3fca13efd723649cb7f10aa8f7b5861d
karson1107 <karson1107@gmail.com>
2023-03-26 15:54:23 +0800

    chatbox update commit

--- home.php
+++ home.php
@@ -14,6 +14,13 @@ if (isset($_SESSION["name"]) && ($_SESSION["user_id"])) {
 		<div class="main">
 		<p class="sign" align="center">Home</p>  
 		<h1> Hello, <?php echo $_SESSION['name']; ?></h1>
+		<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+		<df-messenger
+			intent="WELCOME"
+			chat-title="ABC_School_Chat_Bot"
+			agent-id="1b36ddc5-9aa6-41ab-aacf-360b522648fb"
+			language-code="en"
+		></df-messenger>
         <p class="logout" align="center"><a href="logout.php">Logout</p>		
 		</div>
 		</div>
